6639483638b6f7b245d365e6d9997b2d5641ed6e
David Domingo <ddoming824@gmail.com>
2022-01-19 15:43:39 -0800

    fixed pawn

--- w4d2/chess_game/pawn.rb
+++ w4d2/chess_game/pawn.rb
@@ -5,24 +5,33 @@ class Pawn < Piece
     attr_reader :symbol
     def initialize(color, board, pos)
       super
-      if color == white
-        if pos[0] == 6
-          @move_dirs = [[0, -1], [0, -2]]
+      @symbol = ♙ if color == :white
+      @symbol = ♟ if color == :black
+    end
+
+    def at_start_row?
+        pos[0] == 6 || pos[0] == 1
+    end
+
+    def forward_dir
+        color == :black ? 1 : -1
+    end
+
+    def forward_steps
+        if at_start_row?
+            [[forward_dir, 0], [forward_dir*2, 0]]
         else
-          @move_dirs = [[0, -1]] 
+            [forward_dir, 0]
         end
-      else
-        if pos[0] == 1
-          @move_dirs = [[0, 1], [0, 2]]
-        else
-          @move_dirs = [[0, 1]]
-        end
-      end
-      @symbol = :pawn
+    end
+
+    def side_attacks
+        attack_pos = [ [forward_dir, 1], [forward_dir,-1]]
+        valid_attacks = attack_pos.select do |move|
+            if board[move]
     end
     
 
-    ♙  ♟
-  
+
   
   end
