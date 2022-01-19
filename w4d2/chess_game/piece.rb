e037906a7c39af63dddd6c415e4d129f358a6e3b
David Domingo <ddoming824@gmail.com>
2022-01-19 15:59:25 -0800

    updated piece

--- w4d2/chess_game/piece.rb
+++ w4d2/chess_game/piece.rb
@@ -15,7 +15,18 @@ class Piece
     board[pos].symbol == :null
   end
 
-  
+  def valid_moves
+  end
+
+  def pos=(val)
+  end
+
+  def symbol
+  end
+
+  private 
+  def move_into_check?(end_pos)
+  end
 end
 
 
