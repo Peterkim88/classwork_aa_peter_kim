0ea76fa683f47c486312ed196848c7316348fd15
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-01-24 17:26:27 -0500

    w5d1 5.26pm

--- w5d1/skeleton/lib/p01_int_set.rb
+++ w5d1/skeleton/lib/p01_int_set.rb
@@ -1,19 +1,36 @@
 class MaxIntSet
+
+  attr_reader :store
+
   def initialize(max)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    if is_valid?(num) && !include?(num)
+      @store[num] = true
+    end
   end
 
   def remove(num)
+    if is_valid?(num) && include?(num)
+      @store[num] = false
+    end
   end
 
   def include?(num)
+    return @store[num]
   end
 
   private
 
   def is_valid?(num)
+    if num > @store.length
+      raise "Out of bounds"
+    elsif num < 0
+      raise "Out of bounds"
+    end
+    return true
   end
 
   def validate!(num)
@@ -27,12 +44,28 @@ class IntSet
   end
 
   def insert(num)
+    @store[num % num_buckets] << num
   end
 
   def remove(num)
+    # @store[num % num_buckets].delete(num) if include?(num)
+    if include?(num)
+      i = 0
+      while i < @store[num % num_buckets].length
+        if num == @store[num % num_buckets][i]
+          @store[num % num_buckets].delete_at(i)
+        end
+        i += 1
+      end
+    end
   end
 
   def include?(num)
+    if @store[num % num_buckets].include?(num)
+      return true
+    else
+      return false
+    end
   end
 
   private
