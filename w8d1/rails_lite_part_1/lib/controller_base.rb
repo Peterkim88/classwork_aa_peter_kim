01372635c60f254c21c4cc0e0c454b3b81ed4395
amoxley <alex.moxley@gmail.com>
2022-02-14 16:02:41 -0800

    adds content for w8d1

--- w8d1/rails_lite_part_1/lib/controller_base.rb
+++ w8d1/rails_lite_part_1/lib/controller_base.rb
@@ -2,32 +2,64 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'rack'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params={})
+    @req, @res = req, res
+    @params = route_params.merge(req.params)
+    @already_built_response = false
+    @@protect_from_forgery ||= false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    perpare_render_or_redirect
+    
+    @res.status = 302
+    @res['Location'] = url
+
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    perpare_render_or_redirect
+
+    @res.write(content)
+    @res['Content-Type'] = content_type
+
+    nil
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..", 
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
+
+  render(:index)
 
   # method exposing a `Session` object
   def session
