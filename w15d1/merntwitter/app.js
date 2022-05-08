5ecfeed681fe74c1262efa2d54e2f6ab9b81ba55
Peterkim88 <hyunilpeter.kim@gmail.com>
2022-05-08 14:29:56 -0400

    create jwt token

--- w15d1/merntwitter/app.js
+++ w15d1/merntwitter/app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
-
-app.get("/", (req, res) => res.send("This is awesome!!!"));
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const User = require('./models/User');
 const db = require('./config/keys').mongoURI;
 
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => console.log("Connected to MongoDB successfully"))
-//   .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-  const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = "mongodb+srv://dev:peterkim@merntwitterpeterkim.wscqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-  });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
